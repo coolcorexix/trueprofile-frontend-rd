81db3bc5d8727db52d57ae754347e52396b02f0d
chipueatfast <15520604@gm.uit.edu.vn>
2020-03-27 17:04:59 +0700

    use case change outside workspaces

--- which_to_build.sh
+++ which_to_build.sh
@@ -1,13 +1,15 @@
-current_commit="$(git log -1 --pretty=format:"%h" ./projects)"
+current_commit_of_monorepo="$(git log -1 --pretty=format:"%h")"
+current_commit_of_workspaces="$(git log -1 --pretty=format:"%h" ./projects)"
 
 PROJECTS=("projects/main" "projects/test" "projects/validate")
 
-for p in ${PROJECTS[@]}; do
+for p in ${PROJECTS[@]}; do 
     str="$(git log -1 --pretty=format:"%h" $p)"
-    if [ $current_commit = $str ]
+    if [[ $current_commit_of_workspaces != $current_commit_of_monorepo || \
+        $str = $current_commit_of_workspaces ]]
         then
-            printf "true \n"
+            printf "%s: true \n" $p
         else
-            printf "false \n"
+            printf "%s: false \n" $p
     fi
 done
